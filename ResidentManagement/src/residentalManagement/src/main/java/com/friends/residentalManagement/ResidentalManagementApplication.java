d994b87a6af13cf5009d0bb36494b0842ddd5025
YeuThuyr <thien@LAPTOP-F1GRRMUV>
2025-09-30 15:29:44 +0700

    testing

---
+++ ResidentManagement/src/residentalManagement/src/main/java/com/friends/residentalManagement/ResidentalManagementApplication.java
@@ -0,0 +1,13 @@
+package com.friends.residentalManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResidentalManagementApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ResidentalManagementApplication.class, args);
+	}
+
+}
---
+++ ResidentManagement/src/residentalManagement/src/test/java/com/friends/residentalManagement/ResidentalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friends.residentalManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResidentalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
