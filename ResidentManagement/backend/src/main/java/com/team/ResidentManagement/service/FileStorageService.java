88a0c16f06740fd878afc710f625c63020289e59
YeuThuyr <thienstyle2k5@gmail.com>
2025-11-20 09:28:55 +0700

    File update and redis.

--- ResidentManagement/backend/src/main/java/com/team/ResidentManagement/service/FileStorageService.java
+++ ResidentManagement/backend/src/main/java/com/team/ResidentManagement/service/FileStorageService.java
@@ -18,8 +18,6 @@ import java.nio.file.Paths;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
